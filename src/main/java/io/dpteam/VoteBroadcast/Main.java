95549b8e8b5604f87da4916f82dd8da9347ca588
DartPower <>
2020-12-22 20:35:15 +0300

    Init

--- src/main/java/io/dpteam/VoteBroadcast/Main.java
+++ src/main/java/io/dpteam/VoteBroadcast/Main.java
@@ -6,20 +6,20 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-   public Main() {
-      super();
-   }
+	public Main() {
+		super();
+	}
 
-   public void onEnable() {
-      this.getLogger().info("[DPT.MC] VoteBroadcast Enabled");
-   }
+	public void onEnable() {
+		this.getServer().getLogger().info("[DPT.MC] VoteBroadcast Enabled");
+	}
 
-   public void onDisable() {
-      this.getLogger().info("[DPT.MC] VoteBroadcast Disabled");
-   }
+	public void onDisable() {
+		this.getServer().getLogger().info("[DPT.MC] VoteBroadcast Disabled");
+	}
 
-   public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
-      sender.sendMessage(ChatColor.AQUA + "> " + ChatColor.RED + "> " + ChatColor.AQUA + "> " + ChatColor.BOLD + ChatColor.AQUA + " Voting links:\n" + ChatColor.GOLD + "link1\n" + ChatColor.GOLD + "link2\n" + ChatColor.GOLD + "link3\nlink4");
-      return true;
-   }
+	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+		sender.sendMessage(ChatColor.AQUA + "> " + ChatColor.RED + "> " + ChatColor.AQUA + "> " + ChatColor.BOLD + ChatColor.AQUA + " Voting links:\n" + ChatColor.GOLD + "link1\n" + ChatColor.GOLD + "link2\n" + ChatColor.GOLD + "link3\nlink4");
+		return true;
+	}
 }
